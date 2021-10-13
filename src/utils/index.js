4d8af98e1f9e8f4c356b0a93433b305b8682732b
Xiaorunrun <1976457550@qq.com>
2021-10-13 20:14:53 +0800

    对百度地图API的使用添加一些注释

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 // 导入axios
 import axios from 'axios'
+import {BASE_URL} from './url'
 
 // 创建并导出获取定位城市的函数 getCurrentCity
 export const getCurrentCity = () => {
@@ -10,10 +11,24 @@ export const getCurrentCity = () => {
     if (!localCity) {
         // 如果没有，就使用首页中获取定位城市的代码来获取，并且存储到本地存储中，然后返回该城市数据
         return new Promise((resolve, reject) => {
+            // 使用百度地图API IP 定位方式，获取当前所在城市
             const curCity = new window.BMapGL.LocalCity()
+            // 或者使用浏览器进行定位
+            // const curCity = new window.BMapGL.Geolocation()
+            // curCity.getCurrentPosition(async res => {
             curCity.get(async res => {
                 try {
-                    const result = await axios.get(`http://localhost:8009/area/info?name=${res.name}`)
+                    // res.name 就是当前城市名称, 而 /area/info?name=res.name 是后端提供的一个方法，用于获取城市的详细信息
+                    /*
+                    例如，传入的城市名称为“北京”，后端返回的body为以下内容
+                      "body": {
+                            "label": "北京",
+                            "value": "AREA|88cff55c-aaa4-e2e0"
+                        }
+                    */
+                    const result = await axios.get(`${BASE_URL}/area/info?name=${res.name}`)
+                    // console.log(result);
+                    
                     // 存储到本地存储中
                     localStorage.setItem('zfy_city', JSON.stringify(result.data.body))
                     // 返回该城市数据
